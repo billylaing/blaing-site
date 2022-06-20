604e48a76b5900d39d2aa951748839b0c1a96778
billy laing <blaing@blaing.io>
2022-06-20 12:30:44 -0400

    Clean up README and set some styles

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold text-center">
-      blaing consulting
-    </h1>
+    <figure className="md:flex bg-slate-100 p-8 md:p-0 dark:bg-slate-800">
+        <div className="pt-6 md:p-8 text-center md:text-left space-y-0">
+            <div className="text-sky-500 dark:text-sky-400">
+              BLAING CONSULTING
+            </div>
+            <div className="text-slate-700 dark:text-slate-500">
+              Software Architecture and Infrastructure
+            </div>
+        </div>
+    </figure>
   )
 }
