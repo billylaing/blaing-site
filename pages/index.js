87641445d8cc271c71cca3ed7890125c2fb9dd69
billy laing <blaing@blaing.io>
2022-06-20 12:57:33 -0400

    Small style tweaks

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,18 @@
 export default function Home() {
   return (
-    <figure className="md:flex bg-slate-100 p-8 md:p-0 dark:bg-slate-800">
-        <div className="pt-6 md:p-8 text-center md:text-left space-y-0">
-            <div className="text-sky-500 dark:text-sky-400">
-              BLAING CONSULTING
-            </div>
-            <div className="text-slate-700 dark:text-slate-500">
-              Software Architecture and Infrastructure
-            </div>
-        </div>
+    <figure className="md:flex bg-slate-100 p-8 md:p-0">
+      <div className="pt-6 md:p-8 text-center md:text-left space-y-0">
+        <h1 className={""}>
+          BLAING CONSULTING
+        </h1>
+        <p className={"text-slate-400"}>
+          Software Architecture and Infrastructure
+        </p>
+        <p className={"text-slate-400"}>
+          Terraform, AWS (Glue, ECS, Step Functions), Vercel...
+        </p>
+      </div>
+
     </figure>
   )
 }
